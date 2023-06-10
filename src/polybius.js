4c7dfab052188a3948819c8f22c04775be78b915
soonenough <128852566+soonenough@users.noreply.github.com>
2023-06-10 10:55:26 -0500

    Update polybius.js

--- src/polybius.js
+++ src/polybius.js
@@ -6,9 +6,10 @@
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
-  const alphabet = ['a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
-  const numeric = ['11','21','31','41','51','12','22','32','42','52','13','23','33','43','53','14','24','34','44','54','15','25','35','45','55','  '];
+  const alphabet = ['a','b','c','d','e','f','g','h','i/j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+  const numeric = ['11','21','31','41','51','12','22','32','42','52','13','23','33','43','53','14','24','34','44','54','15','25','35','45','55',' '];
   const decoder = ['a','b','c','d','e','f','g','h','i/j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '];
+
   //iterate through input
   function polybius(input, encode = true) {
     //lowercase input
@@ -16,34 +17,31 @@ const polybiusModule = (function () {
     let decoded = [];
     let lowerCode = input.toLowerCase();
     let lowerCodeNoSpaces = lowerCode.replace(" ","  ");
-    //let lowerCodeNoSpaces = lowerCode;
 
     if(!encode === true){
       if(lowerCodeNoSpaces.length % 2 != 0){
         return false;
       }
+
       //split string by spaces
-      //group each character by 2 and push to grouped array
-       const groupedString = lowerCodeNoSpaces.match(/.{1,2}/g) ?? [];
-      //const groupedString = lowerCodeNoSpaces
-      // console.log(groupedString);
+      const groupedString = lowerCodeNoSpaces.match(/.{1,2}/g) || [];
+
       //iterate through grouped array
       for(let i = 0; i < groupedString.length; i++){
-      //translate numbers(numIndex) using index to alphabet(alphaIndex)
-      let character = groupedString[i];
-      let numIndex = numeric.indexOf(character)
-      //push new characters to decoded array
-      if(groupedString[i] >= 0){
-        decoded.push(decoder[numIndex]);
-      } else {
-        decoded.push(character)
+        //translate numbers (numIndex) using index to alphabet (alphaIndex)
+        let character = groupedString[i];
+        let numIndex = numeric.indexOf(character);
+
+        //push new characters to decoded array
+        if(numIndex >= 0){
+          decoded.push(decoder[numIndex]);
+        } else {
+          decoded.push(character);
+        }
       }
-      }
-      //console.log(decoded);
+
       let finalDecoded = decoded.join("");
       return finalDecoded.replace(/\s\s/g, " ");
-
-
     } else {
       //encode
       for(let j = 0; j < lowerCode.length; j++){
@@ -58,11 +56,12 @@ const polybiusModule = (function () {
       let newOutput = output.join("");
       return newOutput.replace("j", 42);
     }
-}
+  }
 
   return {
     polybius,
   };
 })();
+
 
 module.exports = { polybius: polybiusModule.polybius };
